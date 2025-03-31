d74815aec8005848577a607c549087fb43676004
marcardus <marcardus@gmail.com>
2025-03-31 12:35:18 +0200

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,82 @@
-function CountryDetails() {}
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import axios from "axios"
+
+function CountryDetails() {
+    // console.log(useParams());
+    const { countryId } = useParams();
+
+    const [oneCountry, setOneCountry] = useState(null)
+    const [countries, setCountries] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const getData = async () => {
+      
+      try {
+        setIsLoading(true)
+        const countriesResponse = await axios.get("https://ih-countries-api.herokuapp.com/countries")
+        // console.log(countriesResponse.data);
+        setCountries(countriesResponse.data)
+        const oneCountriesResponse = await axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+        // console.log(oneCountriesResponse.data);
+        setOneCountry(oneCountriesResponse.data)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error);
+        setIsLoading(true)
+      }
+    }
+
+    useEffect(() => {
+      getData()
+    }, [countryId])
+
+    if(isLoading) {
+      return <ClipLoader />
+    }
+
+    return (
+        <div className="container">
+            <p style={{ fontSize: "24px", fontWeight: "bold" }}>
+                Country Details
+            </p>
+
+            <h1>{oneCountry.name.common}</h1>
+
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }}>Capital</td>
+                        <td>{oneCountry.capital[0]}</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            {oneCountry.area} km
+                            <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                              {oneCountry.borders.map((eachBorder, index) => {
+                                return <li key={index}><Link to={`/${eachBorder}`} style={{ textDecoration: 'none', marginLeft: '10px' }}>
+                                  {(countries.find(el => el.alpha3Code === eachBorder)).name.common}
+                                </Link></li>
+                              })}
+                                
+                                
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,49 @@
-function HomePage() {}
+import { useEffect, useState } from "react";
+import axios from "axios"
+import { Link } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
+function HomePage() {
+    const [countries, setCountries] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const getData = async () => {
+      try {
+          const response = await axios.get("https://ih-countries-api.herokuapp.com/countries");
+          console.log(response);
+          setCountries(response.data)
+          setIsLoading(false)
+      } catch (error) {
+        console.log(error);
+        setIsLoading(true)
+      }
+    }
+
+    useEffect(() => {
+      getData()
+    }, [])
+
+    if (isLoading) {
+      return <ClipLoader color={"#d63636"}/>
+    }
+
+    return (
+        <div className="container" style={{maxHeight: "90vh", overflow: "scroll"}}>
+        <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
+
+        <div className="list-group">
+          {countries.map(eachCountry => {
+            return <Link key={eachCountry._id} className="list-group-item list-group-item-action" to={`/${eachCountry.alpha3Code}`}>
+             <img src={`https://flagpedia.net/data/flags/icon/72x54/${eachCountry.alpha2Code.toLowerCase()}.png`} alt={"flag"}/>
+             <p>{eachCountry.name.common}</p>
+            </Link>
+          })}
+          
+        </div>
+      </div>
+    )
+}
+
 
 export default HomePage;
